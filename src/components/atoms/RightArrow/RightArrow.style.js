57b922bdcc2b3c28336f250edd88533fcd983913
bjethi <bikramjethi@gmail.com>
2019-01-27 22:38:27 +0530

    Basic layout Completed

--- src/components/atoms/RightArrow/RightArrow.style.js
+++ src/components/atoms/RightArrow/RightArrow.style.js
@@ -1,11 +1,11 @@
 import { css } from "styled-components";
 
 export default css`
-  .left-arrow {
+  .right-arrow {
     border-top: 3px solid #000;
-    border-left: 3px solid #000;
+    border-right: 3px solid #000;
     min-width: 10px;
     min-height: 10px;
-    transform: rotate(-45deg);
+    transform: rotate(45deg);
   }
 `;
